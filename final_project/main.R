496cf4ff4cedae4951d234aff3fac132a7e7bec9
Vasiliy Ostapenko <ostapenkovs@pm.me>
2022-04-12 18:37:32 -0700

    hw3 done

--- final_project/main.R
+++ final_project/main.R
@@ -1,4 +1,6 @@
 library(tidyverse)
+library(plyr)
+library(data.table)
 
 DATA_FOLDER = "./2021VAERSData"
 DATA_FNAME = file.path(DATA_FOLDER, "2021VAERSDATA.csv")
@@ -8,3 +10,10 @@ VACCINE_FNAME = file.path(DATA_FOLDER, "2021VAERSVAX.csv")
 data = read.csv(DATA_FNAME)
 symptoms = read.csv(SYMPTOMS_FNAME)
 vaccine = read.csv(VACCINE_FNAME)
+
+myo_indices = sapply(symptoms[c("SYMPTOM1", "SYMPTOM2", "SYMPTOM3", "SYMPTOM4", "SYMPTOM5")],
+                     grep, pattern="myocarditis", ignore.case=TRUE) %>% Reduce(f=union)
+symptoms = symptoms[myo_indices, ] %>% copy()
+symptoms = symptoms[!duplicated(symptoms$VAERS_ID), ] %>% copy()
+
+vaccine = vaccine[vaccine$VAX_TYPE == "COVID19", ] %>% copy()
