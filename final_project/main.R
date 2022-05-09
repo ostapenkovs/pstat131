c1aa832139e04a76461d14b230dfa664127832f3
Vasiliy Ostapenko <ostapenkovs@pm.me>
2022-05-09 10:28:07 -0700

    hw5 resources

--- final_project/main.R
+++ final_project/main.R
@@ -4,4 +4,6 @@ library(data.table)
 DATA_FOLDER = "./data"
 COMBINED_FNAME = file.path(DATA_FOLDER, "combined.csv")
 
-df = read.csv(COMBINED_FNAME)
+df = read.csv(COMBINED_FNAME) %>% column_to_rownames("vaersId")
+
+print(table(df$myocarditis, df$covid))
