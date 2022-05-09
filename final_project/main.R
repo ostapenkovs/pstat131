29545a892228534606b903ee3bddf76011c954de
Vasiliy Ostapenko <ostapenkovs@pm.me>
2022-05-09 12:00:05 -0700

    viz

--- final_project/main.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-library(data.table)
-
-DATA_FOLDER = "./data"
-COMBINED_FNAME = file.path(DATA_FOLDER, "combined.csv")
-
-df = read.csv(COMBINED_FNAME) %>% column_to_rownames("vaersId")
-
-print(table(df$myocarditis, df$covid))
